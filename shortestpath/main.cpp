ef566226ff7ade8c4f385190db86dda978450ff7
yubinhsieh <hsiehyubin@gmail.com>
2020-12-23 00:32:58 +0800

    Initial Commit

---
+++ shortestpath/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  shortestpath
+//
+//  Created by 謝育斌 on 2020/12/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
